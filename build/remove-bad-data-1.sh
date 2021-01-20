ac5f86cd4b6059e2b7081814b9e40be97cb94057
Manyama, Jaron <jaron.manyama@concur.com>
2021-01-20 09:58:51 -0800

    update

--- build/remove-bad-data-1.sh
+++ build/remove-bad-data-1.sh
@@ -1,4 +1,5 @@
 
+cd ./src/Slate-API-Explorer-Reference/slate/source/includes/
 
 for thefile in *.json.md ; do
    grep -v "<th>Scope</th>" $thefile > $thefile.$$.tmp
--- build/remove-bad-data-2.sh
+++ build/remove-bad-data-2.sh
@@ -1,4 +1,5 @@
 
+cd ./src/Slate-API-Explorer-Reference/slate/source/includes/
 
 for thefile in *.json.md ; do
    grep -v "<th>Scope Definition</th>" $thefile > $thefile.$$.tmp
