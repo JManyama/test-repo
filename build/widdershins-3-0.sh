e287ac8d0ae2c733e89c54bde1a22da652bf7914
Manyama, Jaron <jaron.manyama@concur.com>
2021-01-21 22:31:09 -0800

    update

--- build/widdershins-3-0.sh
+++ build/widdershins-3-0.sh
@@ -1,3 +1,3 @@
 cd ./src/api-explorer/v4-0/
 
-sudo widdershins _Budget.swagger2.json -o _Budget.swagger2.json.md --user_templates "./src/api-explorer/templates/openapi3-copy"
+sudo widdershins _Budget.swagger2.json -o _Budget.swagger2.json.md --user_templates "MY_ENV_VAR"
